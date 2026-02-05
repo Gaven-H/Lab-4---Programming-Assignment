7a5aa94e4d57e2b3a4357481963837a257fcef45
Bemst <gahuforschool@gmail.com>
2026-02-05 18:38:12 -0500

    User input + win condition + challenge

--- Hangman.py
+++ Hangman.py
@@ -76,3 +76,18 @@ game_state = True
 print ("Guess the number including and between 1-15!")
 print ("Each wrong guess takes you one step closer to avoid an untimely end...")
 print ("Worry not, you will be given mysterious assistance from beyond!")
+
+while game_state == True:
+    user_guess = input("Please enter your guess: ")
+    user_guess = int(user_guess)
+
+    if user_guess == num_to_guess:
+        print("Congratulations! You guessed the number, crisis averted!")
+        game_state = False
+    
+    else:
+        if user_guess < num_to_guess:
+            print ("Number too low!")
+        
+        else:
+            print("Number too high!")
