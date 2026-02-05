831273b01c6c9be8c024a38a4128ff3d34b35fe5
Bemst <gahuforschool@gmail.com>
2026-02-05 18:27:05 -0500

    User instructions

--- Hangman.py
+++ Hangman.py
@@ -68,3 +68,7 @@ ________
 ===========
 """
 ]
+
+print ("Guess the number including and between 1-15!")
+print ("Each wrong guess takes you one step closer to avoid an untimely end...")
+print ("Worry not, you will be given mysterious assistance from beyond!")
