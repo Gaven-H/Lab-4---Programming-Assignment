0ec786489e4eeaa90f7a05158ab9696abeff4e2c
Bemst <gahuforschool@gmail.com>
2026-02-05 18:29:27 -0500

    Wrong guess counter and game state variable

--- Hangman.py
+++ Hangman.py
@@ -69,6 +69,10 @@ ________
 """
 ]
 
+wrong_guess_counter = 0
+
+game_state = True
+
 print ("Guess the number including and between 1-15!")
 print ("Each wrong guess takes you one step closer to avoid an untimely end...")
 print ("Worry not, you will be given mysterious assistance from beyond!")
